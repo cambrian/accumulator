2f0b54931998a2c677269f5ecf6576699809938f
Eddie Wang <eddie@vest.com>
2019-01-15 17:16:12 -0800

    add ring dependency

--- src/group/rsa.rs
+++ src/group/rsa.rs
@@ -1,6 +1,19 @@
-use bigint::uint::U128;
+// use bigint::uint::U256;
 use alga::general::MultiplicativeGroup;
+use num::One;
+use std::ops::Mul;
 
-struct RSA<T>(T); // represents RSA group over elements of type T. Hard-coded modulus for now.
+// TODO: use ring here
 
-impl MultiplicativeGroup for RSA<U128> {}
+// struct RSA<T>( T); // represents RSA group elements of type T. Hard-coded modulus for now.
+
+// impl Mul for RSA<u128> {
+//   fn mul(self, rhs: RSA<u128>) -> RSA<u128> { RSA::<u128>(self * rhs) }
+// }
+
+// impl<T> One for RSA<T> where T: One, RSA<T>: Mul {
+//   fn one() -> RSA<T> { RSA::<T>(T::one()) }
+// }
+
+// impl MultiplicativeGroup for RSA<u128> {
+// }
