598390a29aa21880a88ed8029d570a887222d506
Lucas Ege <lucas@vest.com>
2019-01-16 12:34:13 -0800

    commit to pull

--- src/proof/poe.rs
+++ src/proof/poe.rs
@@ -1,17 +1,42 @@
 // TODO
+use super::super::group::Pow;
+use super::PoE;
+use alga::general::AbstractGroup;
+use alga::general::Operator;
+use bigint::uint::U256;
+use num::BigUint;
 
 // x, u, w: u^x = w
-// pub fn compute_poe<G: Group> (G base, U256 exp, G result) -> PoE<G> {
-//   let l = Hprime (exp, base, result);
-//   let q = (exp / l);
-//   PoE<G>(base^q)
-// }
+pub fn compute_poe<O, G: AbstractGroup<O> + Pow<O>>(base: G, exp: U256, result: G) -> PoE<G>
+where
+  O: Operator,
+{
+  // TODO: Use HPrime function when defined
+  let l = U256::from(1); // HPrime(exp, base, result);
+  let q = exp / l;
+  // TODO: Convert q U256 result into BigUint
+  let q = BigUint::from(23 as u8);
+  PoE { q: base.pow(q) }
+}
 
-// pub fn verify_poe<G: Group> (G base, U256 exp, G result, PoE<G> proof) -> Bool{
+pub fn verify_poe<O, G: AbstractGroup<O> + Pow<O>>(
+  base: G,
+  exp: U256,
+  result: G,
+  proof: PoE<G>,
+) -> bool
+where
+  O: Operator,
+{
+  // TODO: Use HPrime function when defined
+  let l = U256::from(1); // HPrime(exp, base, result);
+  let r = exp % l;
+  let l = BigUint::from(23 as u8); // BigInt::from(l as U256);
+  let w = proof.q.pow(l);
+  // let w = w.mul(base.pow(r));
+  w == result
+}
 
-// }
-
-// impl<T> f
 // Prove (x, u, w)
 // l <- Hprime (x, u, w)
 // q <- Int divide x / l
--- src/proof/poke2.rs
+++ src/proof/poke2.rs
@@ -1,2 +1,11 @@
 // TODO
-// pub fn compute_poke2<G: Group>(G base, BigInt exp, G result) -> PoKE2
+use super::PoKE2;
+use num::BigUint;
+
+pub fn compute_poke2<G>(base: G, exp: BigUint, result: G) -> PoKE2<G> {
+  PoKE2 {
+    z: base,
+    q: result,
+    r: exp,
+  }
+}
