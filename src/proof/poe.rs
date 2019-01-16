c31560153c5da0e8903fddb8ada369384bfaa116
Lucas Ege <lucas@vest.com>
2019-01-16 10:35:12 -0800

    PoE and PoKE struct types

--- src/proof/poe.rs
+++ src/proof/poe.rs
@@ -1,1 +1,29 @@
 // TODO
+
+struct PoE<G> {
+  q: G,
+}
+
+// x, u, w: u^x = w
+// pub fn compute_poe<G: Group> (G base, U256 exp, G result) -> PoE<G> {
+//   let l = Hprime (exp, base, result);
+//   let q = (exp / l);
+//   PoE<G>(base^q)
+// }
+
+// pub fn verify_poe<G: Group> (G base, U256 exp, G result, PoE<G> proof) -> Bool{
+
+// }
+
+// impl<T> f
+// Prove (x, u, w)
+// l <- Hprime (x, u, w)
+// q <- Int divide x / l
+// r <- x mod l
+// return PoE <- u^q
+
+// Verify (x, u, w, Q)
+// l <- Hprime (x, u, w)
+// r <- x mod l
+// Check Q^lu^r = w
+// 1 if yes, else 0
--- src/proof/poke2.rs
+++ src/proof/poke2.rs
@@ -1,1 +1,9 @@
 // TODO
+use num::BigUint;
+
+struct PoKE2i<G> {
+  z: G,
+  q: G,
+  r: BigUint,
+}
+// pub fn compute_poke2<G: Group>(G base, BigInt exp, G result) -> PoKE2
