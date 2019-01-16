c558ed278c3cb2dbd5f02108e6729e89aa7eda72
Sanjay Kannan <whaatt@gmail.com>
2019-01-16 10:53:37 -0800

    push setup

--- src/accumulator.rs
+++ src/accumulator.rs
@@ -1,2 +1,10 @@
-// use alga::general::AbstractGroup;
-// use super::group;
+use super::group::Generator;
+use alga::general::AbstractGroup;
+use alga::general::Operator;
+
+pub fn setup<O, G: AbstractGroup<O> + Generator<O>>() -> G
+where
+  O: Operator,
+{
+  G::generator()
+}
