aff218cf1a7fa4e4c333b88a6de9c11c47df0efc
Pablo Aldape <pablo@vest.com>
2019-01-17 12:24:25 -0800

    sha256, cleaned up warnings

--- src/hash/hashes.rs
+++ src/hash/hashes.rs
@@ -1,11 +1,13 @@
+use super::primality::is_prob_prime;
 use blake2_rfc::blake2b::{blake2b, Blake2bResult};
 use num::bigint::BigUint;
-use sha2::Sha256;
+use sha2::{Digest, Sha256};
 
 // 32 bytes = 256 bits.
 const HASH_LENGTH_IN_BYTES: usize = 32;
 
 // optional key can be used as a nonce for data in hash function
+#[allow(dead_code)]
 pub fn blake2(data: &[u8], key: Option<&[u8]>) -> BigUint {
   let key: &[u8] = match key {
     Some(bytes) => bytes,
@@ -16,28 +18,45 @@ pub fn blake2(data: &[u8], key: Option<&[u8]>) -> BigUint {
   BigUint::from_bytes_be(res_bytes)
 }
 
-pub fn sha256(data: &[u8], key: Option<&[u8]>) -> BigUint {
-  unimplemented!();
-  // let mut hasher = Sha256::new();
-  // hasher.input(data);
-  // hasher.result();
+#[allow(dead_code)]
+pub fn sha256(data: &[u8], _key: Option<&[u8]>) -> BigUint {
+  let mut hasher = Sha256::new();
+  hasher.input(data);
+  let res_bytes: &[u8] = &hasher.result()[..];
+  BigUint::from_bytes_be(res_bytes)
 }
 
+#[allow(dead_code)]
 pub fn blake2_prime(data: &[u8]) -> BigUint {
-  unimplemented!()
+  let mut counter = BigUint::from(0u64);
+  loop {
+    let h = blake2(data, Some(&counter.to_bytes_be()));
+    if is_prob_prime(&h) {
+      return h;
+    }
+    counter += 1u64;
+  }
 }
 
 #[test]
 fn test_blake2() {
   let data = b"test";
-  let hash = blake2(data, None);
-  let hash2 = blake2(data, Some(&[1]));
-  assert_ne!(hash, hash2);
+  assert_ne!(blake2(data, None), blake2(data, Some(&[1])));
 }
 
 #[test]
 fn test_sha256() {
   let data = b"hello world";
-  let hash = sha256(data, None);
-  //assert_eq!(hash, b"94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9");
+  let data2 = b"wut";
+  assert_ne!(sha256(data, None), sha256(data2, None));
+  // assert_eq!(
+  //   sha256(data, None),
+  //   b"94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
+  // );
 }
+
+// WIP: benchmarking blake2, sha256, and eventually *_prime
+// #[bench]
+// fn bench_blake2(b: &mut Bencher) {
+//   unimplemented!()
+// }
