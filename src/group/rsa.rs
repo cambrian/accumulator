46ba731a6b216fffa9f3eb12a8d67e1c1d192455
Eddie Wang <eddie@vest.com>
2019-01-18 17:52:23 -0800

    constant time exp draft integration

--- src/group/rsa.rs
+++ src/group/rsa.rs
@@ -1,8 +1,8 @@
 use super::{Group, InvertibleGroup};
 use num::BigInt;
 use num::BigUint;
-use ring::rsa::bigint::{Modulus, Elem as RingElem, elem_mul};
-use ring::arithmetic::montgomery::R;
+use ring::rsa::bigint::{Modulus, Elem as RingElem, elem_mul, PrivateExponent, elem_exp_consttime};
+use ring::arithmetic::montgomery::{Unencoded, R};
 use serde::ser::{Serialize, Serializer};
 use untrusted::Input;
 use std::result::Result;
@@ -20,6 +20,9 @@ lazy_static! {
   };
 }
 
+/// We restrict our group to singly-montgomery-encoded values, to have proper closure.
+/// As a result, it's a pain to do some of the operations (see Group impl). We may want to
+/// make more substantial changes to our Ring fork to alleviate this.
 #[derive (PartialEq, Eq, Clone)]
 struct RSA2048Elem (RingElem<RSA2048, R>);
 
@@ -45,11 +48,22 @@ impl Group for Modulus<RSA2048> {
   fn id_(&self) -> RSA2048Elem {
     RSA2048Elem(self.oneR_elem())
   }
+  // We multiply an unencoded 2 by a doubly-encoded 1 to get a singly-encoded 2. There is no other
+  // way (using only public functions from ring) to get singly-encoded values.
   fn base_elem_(&self) -> RSA2048Elem {
     let unencoded_2 = RingElem::from_be_bytes_padded(Input::from(&[2 as u8]), Self::get()).unwrap();
-    RSA2048Elem(elem_mul(&unencoded_2, Self::get().oneRR().as_ref().clone(), Self::get()))
+    RSA2048Elem(encode(unencoded_2))
   }
   fn op_(&self, RSA2048Elem(a): &RSA2048Elem, RSA2048Elem(b): &RSA2048Elem) -> RSA2048Elem {
     RSA2048Elem(elem_mul(&a, b.clone(), &self))
   }
+  fn exp(RSA2048Elem(a): &RSA2048Elem, n: &BigUint) -> RSA2048Elem {
+    let exponent = PrivateExponent::from_be_bytes_padded(Input::from(n.to_bytes_be().as_slice()), Self::get()).unwrap();
+    let unencoded = elem_exp_consttime(a.clone(), &exponent, Self::get()).unwrap();
+    RSA2048Elem(encode(unencoded))
+  }
+}
+
+fn encode(a: RingElem<RSA2048, Unencoded>) -> RingElem<RSA2048, R> {
+  elem_mul(Modulus::<RSA2048>::get().oneRR().as_ref(), a, Modulus::<RSA2048>::get())
 }
