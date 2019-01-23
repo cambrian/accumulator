3f91697528a007a6b6e90ac194d6ea7498c307ae
Pablo Aldape <pablo@vest.com>
2019-01-23 13:41:40 -0800

    updated benches

--- benches/hash/hashes.rs
+++ benches/hash/hashes.rs
@@ -3,25 +3,20 @@
 extern crate criterion;
 
 use criterion::Criterion;
-use crypto::hash::{blake2, sha256, h_prime};
+use crypto::hash::{Blake2b, hash, hash_to_prime};
 use rand::Rng;
 
 fn bench_blake2() {
-  blake2(b"test", None);
-}
-
-fn bench_sha256() {
-  sha256(b"test", None);
+  hash(&Blake2b::default, b"werg");
 }
 
 fn bench_h_prime() {
   let random_bytes = rand::thread_rng().gen::<[u8; 32]>();
-  h_prime(&blake2, &random_bytes);
+  hash_to_prime(&Blake2b::default, &random_bytes);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
   c.bench_function("blake2", |b| b.iter(|| bench_blake2()));
-  c.bench_function("sha256", |b| b.iter(|| bench_sha256()));
   c.bench_function("h_prime", |b| b.iter(|| bench_h_prime()));
 }
 
