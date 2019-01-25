f6d0b2152087429eed9b844713a1e8ddd1ae5f30
Sanjay Kannan <whaatt@gmail.com>
2019-01-24 17:22:53 -0800

    Add some safety

--- src/group/rsa.rs
+++ src/group/rsa.rs
@@ -46,7 +46,12 @@ impl Group for RSA2048 {
     RSA2048::elem(x.0.clone().invert(modulus).unwrap())
   }
   fn exp_(modulus: &Integer, x: &RSA2048Elem, n: &Integer) -> RSA2048Elem {
-    RSA2048::elem(x.0.clone().pow_mod(n, modulus).unwrap())
+    assert!(*n >= int(0));
+    if n.is_congruent(&int(0), &int(2)) {
+      return RSA2048::elem(x.0.clone().pow_mod(n, modulus).unwrap());
+    }
+    // For odd positive exponents, secure_pow_mod is side-channel resistant.
+    RSA2048::elem(x.0.clone().secure_pow_mod(n, modulus))
   }
 }
 
