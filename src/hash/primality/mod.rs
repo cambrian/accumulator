3957294b1468407f58a5b83fdd7764caa90709e3
Pablo Aldape <pablo@vest.com>
2019-01-25 11:49:26 -0800

    weak -> strong Lucas, cmts

--- src/hash/primality/mod.rs
+++ src/hash/primality/mod.rs
@@ -6,7 +6,9 @@ mod constants;
 pub const MAX_JACOBI_ITERS: u32 = 500;
 
 /// Implements the Baillie-PSW probabilistic primality test, which is known to be deterministic over
-/// all integers up to 64 bits (u64).
+/// all integers up to 64 bits (u64). Offers more bang for your buck than Miller-Rabin (i.e.
+/// iterated Fermat tests of random base) at wide n since Fermat and Lucas pseudoprimes have been
+/// shown to be anticorrelated. Steps of BPSW are as follows:
 /// 1. Accept small primes and reject multiples of them.
 /// 2. Do a single iteration of Miller-Rabin (base-2 Fermat test).
 /// 3. Filter squares.
@@ -59,6 +61,7 @@ pub fn is_square(n: &Integer) -> bool {
 
 /// Strong Lucas probable prime test (NOT the more common Lucas primality test which requires
 /// factorization of n-1). Selects parameters d, p, q according to Selfridge's method.
+/// Cf. https://en.wikipedia.org/wiki/Lucas_pseudoprime
 fn passes_lucas(n: &Integer) -> bool {
   let d = choose_d(&n);
   let p = int(1);
@@ -66,9 +69,25 @@ fn passes_lucas(n: &Integer) -> bool {
   let delta = n.clone() + 1;
 
   // TODO: Extend to stronger test.
-  let (u_delta, _v_delta) = compute_u_and_v_k(&delta, n, &int(1), &p, &p, &q, &d);
+  let (u_delta, v_delta, q_delta_over_2) =
+    compute_lucas_sequences(&delta, n, &int(1), &p, &p, &q, &d);
   // u_delta % n != 0 proves n composite.
-  u_delta == 0
+  if u_delta != 0 {
+    return false;
+  }
+  // Additional check which is not strictly part of Lucas test but nonetheless filters some
+  // composite n for free. See section "Checking additional congruence conditions" on Wikipedia.
+  if !v_delta.is_congruent(&(2*q.clone()), &n) {
+    return false;
+  }
+  // Congruence check which holds for prime n by Euler's criterion.
+  if !q_delta_over_2.is_congruent(&(q.clone() * q.jacobi(&n)), &n) {
+    return false;
+  }
+  // n is either prime or a "strong" Lucas pseudoprime. (The precise meaning of "strong" is not
+  // fixed in the literature.) Procedure can be further strengthened by implementing more tests in
+  // section 6 of [Baillie & Wagstaff 1980], but for now this is TODO.
+  true
 }
 
 /// Finds and returns first D in [5, -7, 9, ..., 5 + 2 * max_iter] for which Jacobi symbol (D/n) =
@@ -91,31 +110,32 @@ fn choose_d(n: &Integer) -> Integer {
   panic!("Could not find d with (d/n) = -1! Perhaps n is square?");
 }
 
-/// Computes the Lucas sequences {u_i(p, q)} and {v_i(p, q)} up to a specified index k in log(k)
-/// time by recursively calculating only the (2i)th and (2i+1)th elements in an order determined by
-/// the binary expansion of k. In the Lucas probabilistic primality test we specify that
-/// d = p^2 - 4q and are generally concerned with the case that k = delta = n+1.
-/// Cf. https://en.wikipedia.org/wiki/Lucas_pseudoprime
-fn compute_u_and_v_k(
-  k: &Integer,
+/// Computes the Lucas sequences {u_i(p, q)} and {v_i(p, q)} up to a specified index k_target in
+/// O(log(k_target)) time by recursively calculating only the (2i)th and (2i+1)th elements in an
+/// order determined by the binary expansion of k. Also returns q^{k/2} (mod n), which is used in
+/// a stage of the strong Lucas test. In the Lucas case we specify that d = p^2 - 4q and set
+/// k_target = delta = n - (d/n) = n + 1.
+fn compute_lucas_sequences(
+  k_target: &Integer,
   n: &Integer,
   u_1: &Integer,
   v_1: &Integer,
   p: &Integer,
   q: &Integer,
   d: &Integer,
-) -> (Integer, Integer) {
-  let k_bits = to_binary(k);
+) -> (Integer, Integer, Integer) {
+  let k_target_bits = to_binary(k_target);
   let mut u_k = u_1.clone();
   let mut v_k = v_1.clone();
   let mut q_k = q.clone();
+  let mut q_k_over_2 = q.clone();
 
   let mod_n = |x: &Integer| {
     let (_quotient, rem) = x.clone().div_rem_euc(n.clone());
     rem
   };
 
-  // Finds k in Z_n with 2k = x (mod n)
+  // Finds t in Z_n with 2t = x (mod n)
   let half = |x: &Integer| {
     if x.is_congruent_u(1, 2) {
       mod_n(&((x.clone() + n) / 2))
@@ -129,17 +149,21 @@ fn compute_u_and_v_k(
   // v_{2k}, respectively. Else if x_i = 1, update to u_{2k+1} and v_{2k+1}. At the end of the loop
   // we will have computed u_k and v_k, with k as given, in log(delta) time.
   let mut k = int(1);
-  for bit in k_bits[1..].chars() {
-    // Compute (u, v)_{2k} from (u, v)_k.
-    // u_2k = u_k * v_k % n
-    // v_2k = v_k^2 - 2*q^k
+  for bit in k_target_bits[1..].chars() {
+    // Compute (u, v)_{2k} from (u, v)_k according to the following:
+    // u_2k = u_k * v_k (mod n)
+    // v_2k = v_k^2 - 2*q^k (mod n)
     u_k = mod_n(&(u_k.clone() * v_k.clone()));
     modpow_inplace(&mut v_k, &int(2), n);
     v_k = mod_n(&(v_k - 2 * q_k.clone()));
+    // Continuously maintain q_k = q^k (mod n) and q_k_over_2 = q^{k/2} (mod n).
+    q_k_over_2 = q_k.clone();
     q_k = mod_n(&(q_k.clone() * q_k.clone()));
     k *= 2;
     if bit == '1' {
-      // Compute (u, v)_{2k+1} from (u, v)_{2k}.
+      // Compute (u, v)_{2k+1} from (u, v)_{2k} according to the following:
+      // u_{2k+1} = 1/2 * (p*u_{2k} + v_{2k}) (mod n)
+      // v_{2k+1} = 1/2 * (d*u_{2k} + p*v_{2k}) (mod n)
       // TODO: Why is mod_n necessary here?
       let pu_plus_v = p * u_k.clone() + v_k.clone();
       let du_plus_pv = mod_n(&(d * u_k.clone() + p * v_k.clone()));
@@ -149,7 +173,7 @@ fn compute_u_and_v_k(
       k += 1;
     }
   }
-  (u_k, v_k)
+  (u_k, v_k, q_k_over_2)
 }
 
 fn to_binary(n: &Integer) -> String {
@@ -208,17 +232,17 @@ mod tests {
   #[test]
   fn test_lucas() {
     // should fail on p = 2
-    for &p in SMALL_PRIMES[1..].iter() {
-      assert!(passes_lucas(&int(p)));
+    for &sp in SMALL_PRIMES[1..].iter() {
+      assert!(passes_lucas(&int(sp)));
+      assert!(!passes_lucas(&(int(sp) * 241)));
     }
-    for &p in MED_PRIMES.iter() {
-      assert!(passes_lucas(&int(p)));
+    for &mp in MED_PRIMES.iter() {
+      assert!(passes_lucas(&int(mp)));
+      assert!(!passes_lucas(&(int(mp) * 5)));
     }
-    for &p in LARGE_PRIMES.iter() {
-      assert!(passes_lucas(&int(p)));
-    }
-    for &n in STRONG_LUCAS_PSEUDOPRIMES.iter() {
-      assert!(passes_lucas(&int(n)));
+    for &lp in LARGE_PRIMES.iter() {
+      assert!(passes_lucas(&int(lp)));
+      assert!(!passes_lucas(&(int(lp) * 7)));
     }
   }
 
