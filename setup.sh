30aa3d66fa339c4d460120a67700aa2a5d0b2ba2
Sanjay Kannan <whaatt@gmail.com>
2019-01-24 23:14:27 -0800

    Add troubleshooting.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
-# NOTE: if your rls hangs at "building", quit VS Code and run:
-#   cargo clean && rustup update
-# After this, reopen VS Code and you should be good.
-
 # Set up the Rust toolchain and modify PATH.
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
