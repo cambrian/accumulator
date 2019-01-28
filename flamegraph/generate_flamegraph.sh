eb76207f300584fa90ebb8b866535cb388a555fb
Eddie Wang <eddie@vest.com>
2019-01-28 17:30:47 -0500

    flame graphs

--- flamegraph/generate_flamegraph.sh
+++ flamegraph/generate_flamegraph.sh
@@ -10,5 +10,6 @@ bench_exec=$(ls -t ../target/release/deps/$1-* | head -1)
 echo "Calling dtrace to run this executable and collect its stack frames."
 sudo nohup dtrace -x ustackframes=100 -c './'$bench_exec -o out.stacks -n 'profile-997 { @[ustack()] = count(); }' >/dev/null 2>&1
 echo "Generating flamegraph."
-./stackcollapse.pl out.stacks | grep closure | ./flamegraph.pl > $1.svg
-echo "Done. Your flamegraph is now at flamegraph/"$1".svg"
+FLAMEGRAPH_FILENAME=$1.flame.svg
+./stackcollapse.pl out.stacks | grep closure | ./flamegraph.pl > $FLAMEGRAPH_FILENAME
+echo "Done. Your flamegraph is now at flamegraph/"$FLAMEGRAPH_FILENAME
