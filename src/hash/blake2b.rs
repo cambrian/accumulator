0408cb79dfe2050526bc482e8153e1c1b6b76ae1
Eddie Wang <eddie@vest.com>
2019-01-29 13:05:55 -0500

    .

--- src/hash/blake2b.rs
+++ src/hash/blake2b.rs
@@ -2,9 +2,9 @@
 //! TODO: Add SHA-256 interface pending further development.
 use super::GeneralHasher;
 use blake2_rfc::blake2b::Blake2b as Blake2b_;
-use std::hash::Hasher;
-use rug::Integer;
 use rug::integer::Order;
+use rug::Integer;
+use std::hash::Hasher;
 
 // 32 bytes = 256 bits.
 const HASH_LENGTH_IN_BYTES: usize = 32;
@@ -20,7 +20,7 @@ impl Default for Blake2b {
 impl Hasher for Blake2b {
   /// We could return a truncated hash but it's easier just to not use this fn for now.
   fn finish(&self) -> u64 {
-    unimplemented!()
+    panic!("Don't use! Prefer finalize(self).")
   }
   fn write(&mut self, bytes: &[u8]) {
     Blake2b_::update(&mut self.0, bytes)
