f4d6871863cf1b0e42c82ff754ee4eda0c5c9f5a
Michael Straka <michaelstraka@Michaels-MacBook-Pro.local>
2019-01-29 19:39:34 -0800

    compiles

--- src/group/class.rs
+++ src/group/class.rs
@@ -1,7 +1,7 @@
 //! Class Group implementation (draft)
 use super::{ElemFrom, Group, UnknownOrderGroup};
 use crate::util::{int, Singleton};
-use rug::Integer;
+use rug::{Assign, Integer};
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum ClassGroup {}
@@ -21,8 +21,8 @@ pub struct ClassElem {
 
 impl ClassElem {
   fn normalize(&mut self) {
-    if (self.b > -(&self.a) && self.b <= self.a) {
-      return
+    if self.b > Integer::from(-&self.a) && self.b <= self.a {
+      return;
     }
     // r = (a - b) / 2a, floor division
     // a = a
@@ -32,13 +32,13 @@ impl ClassElem {
     // TODO: make sure this is floor division
     let r = Integer::from(&self.a - &self.b) / &aa;
 
-    self.c = &self.a * &r * &r + &self.b * &r + self.c;
-    self.b = self.b + 2 * r * &self.a;
+    self.c = Integer::from(&self.a * &r) * &r + &self.b * &r + &self.c;
+    self.b = &self.b + 2 * r * &self.a;
   }
 
   fn reduce(&mut self) {
     self.normalize();
-    while (self.a > self.c || (self.a == self.c && self.b < 0)) {
+    while self.a > self.c || (self.a == self.c && self.b < 0) {
       // s = (c + b) / 2c, floor division
       // a = c
       // b = -b + 2sc
@@ -48,9 +48,9 @@ impl ClassElem {
       let s = Integer::from(&self.c + &self.b) / &cc;
       let old_c = Integer::from(&self.c);
 
-      self.c = self.c * &s * &s - &self.b * &s + &self.a;
-      self.b = -self.b + 2 * &s * &old_c;
-      self.a.assign(old_c);
+      self.c = Integer::from(&self.c * &s) * &s - &self.b * &s + &self.a;
+      self.a.assign(&old_c);
+      self.b = Integer::from(-&self.b) + 2 * Integer::from(&s * &old_c);
     }
     self.normalize();
   }
@@ -59,19 +59,19 @@ impl ClassElem {
 // TODO: Check for solution to congruence?
 pub fn solve_linear_congruence(a: &Integer, b: &Integer, m: &Integer) -> (Integer, Integer) {
   // g = gcd(a, m), da + em = g
-  let (g, d, e) = a.gcd_cofactors(m, Integer::new());
+  let (g, d, _) = a.clone().gcd_cofactors(m.clone(), Integer::new());
   // q = b/g, r = b % g
   // mu = (q * d) % m
   // v = m / g
 
-  let mu = ((&b/&g) * d) % &m;
-  let v = &m / g;
+  let mu = ((b / g.clone()) * d) % m;
+  let v = m / g;
   (mu, v)
 }
 
 #[inline]
 pub fn three_gcd(a: &Integer, b: &Integer, c: &Integer) -> Integer {
-  a.gcd(&b).gcd(&c);
+  a.clone().gcd(&b).gcd(&c)
 }
 
 impl Singleton for ClassGroup {
@@ -83,11 +83,11 @@ impl Singleton for ClassGroup {
 
 impl Group for ClassGroup {
   type Elem = ClassElem;
-  fn op_(discriminant: &Integer, x: &ClassElem, y: &ClassElem) -> ClassElem {
+  fn op_(_: &Integer, x: &ClassElem, y: &ClassElem) -> ClassElem {
     // g = (b1 + b2) / 2
-    let g = (&x.b + &y.b) / 2;
+    let g = (Integer::from(&x.b) + &y.b) / 2;
     // h = (b2 - b1) / 2
-    let h = (&y.b - &x.b) / 2;
+    let h = (&y.b - Integer::from(&x.b)) / 2;
     // w = gcd(a1, a2, g)
     let w = three_gcd(&x.a, &y.a, &g);
     // j = w
@@ -105,7 +105,7 @@ impl Group for ClassGroup {
     // b = hu + sc1
     let b = Integer::from(&h * &u) + (&s * &x.c);
     // m = st
-    let m = Integer::from(&s * &t);
+    let mut m = Integer::from(&s * &t);
 
     let (mu, v) = solve_linear_congruence(&a, &b, &m);
     // a = tv
@@ -115,40 +115,53 @@ impl Group for ClassGroup {
     // m = s
     m.assign(&s);
 
-    let (lambda, sigma) = solve_linear_congruence(&a, &b, &m);
+    let (lambda, _) = solve_linear_congruence(&a, &b, &m);
     // k = mu + v*lambda
     let k = &mu + Integer::from(&v * &lambda);
     // l = (k*t - h) / s
     let l = (Integer::from(&k * &t) - &h) / &s;
-    // m = (tuk - hu - cs) / st
-    let m = (&t * &u * &k - &h * &u - &c * &s) / (&s * &t);
+    // m = (tuk - hu - c1s) / st
+    let m = (Integer::from(&t * &u) * &k - &h * &u - &x.c * &s) / Integer::from(&s * &t);
     // A = st - ru
-    let A = Integer::from(&s * &t) - Integer::from(&r * &u);
+    let a = Integer::from(&s * &t) - Integer::from(&r * &u);
     // B = ju + mr - (kt + ls)
-    let B = Integer::from(&j * &u) + Integer::from(&m * &r) - (Integer::from(&k * &t) + Integer::from(&l * &s));
+    let b = Integer::from(&j * &u) + Integer::from(&m * &r)
+      - (Integer::from(&k * &t) + Integer::from(&l * &s));
     // C = kl - jm
-    let C = Integer::from(&k * &l) - Integer::from(&j * &m);
-    let ret = ClassElem {a: A, b: B, c: C };
+    let c = Integer::from(&k * &l) - Integer::from(&j * &m);
+    let mut ret = ClassElem { a, b, c };
     ret.reduce();
     ret
   }
 
-  fn id_(_: &Integer) -> ClassElem {
-    unimplemented!();
+  fn id_(d: &Integer) -> ClassElem {
+    let a = Integer::from(1);
+    let b = Integer::from(1);
+    // c = (b*b - d) / 4a
+    let c = Integer::from(-d) / 4;
+    ClassElem { a, b, c }
   }
 
-  fn inv_(discriminant: &Integer, x: &ClassElem) -> ClassElem {
-    ClassElem {a: Integer::from(&x.a), b: Integer::from(-(&x.b)), c: Integer::from(&x.c)}
+  fn inv_(_: &Integer, x: &ClassElem) -> ClassElem {
+    ClassElem {
+      a: Integer::from(&x.a),
+      b: Integer::from(-(&x.b)),
+      c: Integer::from(&x.c),
+    }
   }
 
-  fn exp_(discriminant: &Integer, x: &ClassElem, n: &Integer) -> ClassElem {
+  fn exp_(_: &Integer, x: &ClassElem, n: &Integer) -> ClassElem {
     unimplemented!();
   }
 }
 
 impl UnknownOrderGroup for ClassGroup {
-  fn unknown_order_elem_(_: &Integer) -> ClassElem {
-    unimplemented!();
+  fn unknown_order_elem_(d: &Integer) -> ClassElem {
+    let a = Integer::from(2);
+    let b = Integer::from(1);
+    // c = (b*b - d) / 4a
+    let c = Integer::from(-d) / 8;
+    ClassElem { a, b, c }
   }
 }
 
