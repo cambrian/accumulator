b485ec63a1f545f8d24ac3c719eae76144a85e37
Eddie Wang <eddie@vest.io>
2019-01-31 15:35:10 -0500

    .

--- src/hash/primality/mod.rs
+++ src/hash/primality/mod.rs
@@ -29,7 +29,8 @@ pub fn passes_miller_rabin_base_2(n: &Integer) -> bool {
     return true;
   }
   for _ in 1..r {
-    x.pow_mod_mut(&int(2), n).unwrap();
+    x *= x.clone();
+    x %= n;
     if x == 1 {
       return false;
     }
