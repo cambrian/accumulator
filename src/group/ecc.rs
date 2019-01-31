93b96a66aeb67d7048767187541b2b3d62b8bfb1
Lucas Ege <lucas@vest.io>
2019-01-31 11:37:58 -0800

    Adding exp to ecc

--- src/group/ecc.rs
+++ src/group/ecc.rs
@@ -1,8 +1,10 @@
 /// TODO
 use super::Group;
-use crate::util::TypeRep;
+use crate::util::{int, TypeRep};
 use curve25519_dalek::ristretto::RistrettoPoint;
+use curve25519_dalek::scalar::Scalar;
 use curve25519_dalek::traits::Identity;
+use rug::integer::Order;
 use rug::Integer;
 use std::hash::{Hash, Hasher};
 
@@ -10,9 +12,8 @@ use std::hash::{Hash, Hasher};
 pub enum Ed25519 {}
 
 lazy_static! {
-  pub static ref rp: RistrettoPoint = RistrettoPoint::identity();
+  pub static ref MAX_SAFE_INTEGER: Integer = int(2 ^ 256);
 }
-
 /// Derive copy?
 #[derive(Clone, Debug, Eq)]
 pub struct Ed25519Elem(RistrettoPoint);
@@ -24,8 +25,8 @@ impl Hash for Ed25519Elem {
 
 // TODO
 impl PartialEq for Ed25519Elem {
-  fn eq(&self, _other: &Ed25519Elem) -> bool {
-    true
+  fn eq(&self, other: &Ed25519Elem) -> bool {
+    self.0 == other.0
   }
 }
 
@@ -48,13 +49,22 @@ impl Group for Ed25519 {
   }
 
   fn inv_(_: &(), x: &Ed25519Elem) -> Ed25519Elem {
-    // assert(not point at infinity)
     Ed25519Elem(-x.0)
   }
 
-  fn exp_(_: &(), _x: &Ed25519Elem, _n: &Integer) -> Ed25519Elem {
-    // Need to implement Integer -> Scalar, or x * Integer
-    // Ed25519(x.0 * n)
-    unimplemented!();
+  fn exp_(_: &(), x: &Ed25519Elem, n: &Integer) -> Ed25519Elem {
+    let mut remaining = n.clone();
+    let mut digits: [u8; 32] = [0; 32];
+    let mut result = x.clone();
+    while remaining > *MAX_SAFE_INTEGER {
+      MAX_SAFE_INTEGER.write_digits(&mut digits, Order::LsfLe);
+      let factor = Scalar::from_bytes_mod_order(digits);
+      result = Ed25519Elem(result.0 * factor);
+      // TODO: find way to avoid clone
+      remaining -= MAX_SAFE_INTEGER.clone();
+    }
+    remaining.write_digits(&mut digits, Order::LsfLe);
+    let factor = Scalar::from_bytes_mod_order(digits);
+    Ed25519Elem(result.0 * factor)
   }
 }
