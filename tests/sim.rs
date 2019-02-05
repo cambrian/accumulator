823afdae3dd8c0340bc6128d411c322d708e652f
Lucas Ege <lucas@vest.io>
2019-02-05 11:17:09 -0800

    Changing test file structure

--- tests/sim.rs
+++ tests/sim.rs
@@ -1,4 +1,4 @@
 #[test]
 fn sim() {
-  assert!(false);
+  // unimplemented!();
 }
---
+++ tests/simulation/simulation.rs
@@ -0,0 +1,2 @@
+// TODO add common files here- will not be compiled as seperate crates or have section in test
+// output.
