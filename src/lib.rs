073e046c051811a670cdeef41676bc69d818e0f1
alanefl <alanf94@gmail.com>
2019-03-07 14:56:20 -0800

    Update lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! Cryptographic accumulator and vector commitment library, written and maintained by Cambrian
+//! Labs.
+//!
+//! Most users will simply need to write
+//!
+//! `use accumulator::{Accumulator, VectorCommitment};`
+//!
+//! or similar to use the library. However, internal modules are exported as well in case you would
+//! like to use them.
+//!
+//! We have spent significant time optimizing performance. Most accumulator or vector-commitment
+//! functions will bottleneck in hashing to large primes. To alleviate this, we created a
+//! zero-allocation U256 type that uses the low-level `mpn_` functions in GMP, which is used in
+//! `hash_to_prime`. TODO: Benchmark our U256 vs. 256-bit `rug::Integer` vs. Parity U256.
 #![allow(clippy::unknown_clippy_lints)]
 #![allow(clippy::many_single_char_names)]
 #![allow(clippy::empty_enum)]
@@ -8,17 +22,16 @@ extern crate lazy_static;
 #[macro_use]
 extern crate arrayref;
 
+mod accumulator;
+pub use accumulator::*;
+mod vector_commitment;
+pub use vector_commitment::*;
+
 #[macro_use]
 pub mod util;
 
-mod accumulator;
-pub use crate::accumulator::*;
 pub mod group;
 pub mod hash;
 pub mod num;
 pub mod proof;
-
-mod vector_commitment;
-pub use vector_commitment::*;
-mod uint;
-pub use uint::*;
+pub mod uint;
