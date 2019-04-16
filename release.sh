0570cd8d6b757f6a5e494051d47c0d84d2305a50
Sanjay Kannan <whaatt@gmail.com>
2019-04-16 11:47:24 -0700

    jekyll redirect

--- release.sh
+++ release.sh
@@ -5,3 +5,15 @@ cargo test
 rm -rf target/doc
 cargo doc --no-deps
 cp -r target/doc docs
+
+# Site is published at BASE/accumulator.
+# Docs are at BASE/accumulator/docs.
+mv docs/accumulator docs/docs
+
+# Add Jekyll redirect.
+echo '0a
+---
+redirect_from: "/index.html"
+---
+.
+w' | ed docs/docs/index.html
