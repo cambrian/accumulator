cece760f667b84adeb40c96a70efc9028d7ad70d
Sanjay Kannan <whaatt@gmail.com>
2019-04-17 23:06:28 -0700

    Regenerating docs via script.

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["accumulator"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"accumulator/hash/struct.Blake2b.html\" title=\"struct accumulator::hash::Blake2b\">Blake2b</a>",synthetic:false,types:["accumulator::hash::blake2b::Blake2b"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
